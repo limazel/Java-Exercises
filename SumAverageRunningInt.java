3a466ccd87c7a8290a07cd2de6bd416ce3e6700e
Azad Ayhan <azadayhan50@gmail.com>
2025-04-08 16:20:54 +0300

    SumAverageRunningInt (Decision & Loop)

---
+++ SumAverageRunningInt.java
@@ -0,0 +1,17 @@
+public class SumAverageRunningInt {
+    public static void main(String[] args) {
+        int sum = 0;
+        double average;
+        final int LOWERBOUND = 1;
+        final int UPPERBOUND = 100;
+
+        for(int number = LOWERBOUND; number <= UPPERBOUND; number++) {
+            sum += number;
+        }
+
+        average = (double) sum / (UPPERBOUND - LOWERBOUND + 1);
+
+        System.out.println("The sum is " + sum);
+        System.out.println("The average is " + average); 
+    }
+}
