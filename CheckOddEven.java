e2e9548cd8e998e05458b9ef712df839a6b8f440
Azad Ayhan <azadayhan50@gmail.com>
2025-04-08 16:12:02 +0300

    CheckOddEven (if-else)

---
+++ CheckOddEven.java
@@ -0,0 +1,14 @@
+public class CheckOddEven {
+    public static void main(String[] args) {
+        int number = -2;
+        System.out.println("The number is: " + number);
+
+        if(number % 2 == 0) {
+            System.out.println("EVEN");
+        } else {
+            System.out.println("ODD");
+        }
+
+        System.out.println("bye!");
+    }
+}
